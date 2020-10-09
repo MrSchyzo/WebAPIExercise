ff91fd7db88dc164ca5e84e3661787315be874d3
Mr.Schyzo <catapano-marco@mail.com>
2020-10-10 01:52:50 +0200

    Big Bang

---
+++ WebAPIExercise/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebAPIExercise.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string CompanyCode { get; set; }
+    }
+}
